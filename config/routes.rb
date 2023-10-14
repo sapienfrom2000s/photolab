93f9bce9837214a148f25491b3187e35dcc98ba0
brickwall <queenisuseless@gmail.com>
2023-10-14 22:27:02 +0530

    Only expose sign in path
    The idea is to prevent any normal user from creating entries in db.
    The only way to create admins is to do it through console. No
     unnecessary routes is exposed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # Recommended is devise_for :users, skip: [:registrations, :confirmations], skip_helpers: true but I want to see the conflicts
+    devise_for :admins, only: :sessions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
