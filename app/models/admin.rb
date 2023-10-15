811303fcd5022ad15966ffc9136ac8ba8b7431f3
brickwall <queenisuseless@gmail.com>
2023-10-16 00:14:41 +0530

    Remove lockable to make it work
    it was throwing some error with lockable so i just removed it, will
    see what error it was at a later stage, not important right now.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,6 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :rememberable, :validatable, :lockable, :trackable
+  devise :database_authenticatable,
+         :rememberable, :validatable, :trackable
 end
