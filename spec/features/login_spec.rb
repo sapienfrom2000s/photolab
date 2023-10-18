482af810da66a43671ea2e7dea08b71b9b2fb595
brickwall <queenisuseless@gmail.com>
2023-10-18 15:33:36 +0530

    Add test: denies login with invalid credentials
    - flash centered
    - dried test
    - added flash to layout

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Login", type: :feature do
 
-    let(:admin) { create(:admin) }
+ let(:admin) { create(:admin) }
+
+ before :each do
+   visit new_admin_session_path
+ end
 
  it 'allows admin to sign in with valid credentials' do
-   visit new_admin_session_path
 
    fill_in 'Email', :with=> admin.email
    fill_in 'Password', :with=> admin.password
@@ -14,5 +17,14 @@ RSpec.describe "Login", type: :feature do
 
    expect(page).to have_content('Admin signed in successfully')
    expect(page).to have_link('Log Out')
+ end
+
+ it 'denies to sign in with invalid crendtials' do
+   fill_in 'Email', :with=> 'email@invalid.com'
+   fill_in 'Password', :with=> 'arandompassword'
+
+   click_button 'Log in'
+
+   expect(page).to have_content('Invalid Email or password')
  end
 end
