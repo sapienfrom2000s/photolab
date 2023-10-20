6a79133d40906c5c42a83a80fd446d53123688a5
brickwall <queenisuseless@gmail.com>
2023-10-21 00:30:49 +0530

    seed admin data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Admin.create(email: 'bla@bla.com', password: 'password')
